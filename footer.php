55e826342e9e97d0db6ec63eb5f73e144a623c71
googaloo <rustin.l.odom@gmail.com>
2014-11-30 21:20:21 -0700

    basic setup

---
+++ footer.php
@@ -0,0 +1,17 @@
+
+			<footer></footer>
+
+			<h3>Footer</h3>
+
+		</div>
+
+	</div>
+
+</div>
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="js/foundation.min.js"></script>
+<script type="text/javascript" src="script.js"></script>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Be Aware - Take the test now!</title>
+	<link rel="stylesheet" type="text/css" href="css/foundation.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+<div class="container">
+
+	<div class="row">
+		
+		<nav class="masthead large-12 large-centered">
+			
+			<ul>
+				
+				<li>Home</li>
+				<li>Sign up for FREE</li>
+				<li>FAQ</li>
+
+			</ul>
+
+		</nav>
+
+	</div>
+
+	<div class="row">
+
+		<div class="large-12 large-centered columns">
+
+			<h1>Header</h1>
